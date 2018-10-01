2c604f6d285efa13295d7e6ab217f99e7e50e2dd
selametsamli <selamet96@gmail.com>
2018-10-01 12:44:49 +0300

    created registerform

--- OnlineMarkdownEditor/OnlineMarkdownEditor/settings.py
+++ OnlineMarkdownEditor/OnlineMarkdownEditor/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'user',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -119,3 +120,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
--- OnlineMarkdownEditor/user/views.py
+++ OnlineMarkdownEditor/user/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render, redirect
+from .forms import RegisterForm
 
 # Create your views here.
+
+def register(request):
+    form = RegisterForm()
+    context = {
+        "form":form
+
+    }
+
+    return render(request,"register.html",context)
+
+def login(request):
+    pass
+def logout(request):
+    pass
