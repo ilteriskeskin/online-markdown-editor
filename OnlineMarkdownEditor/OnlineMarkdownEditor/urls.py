b1d8f2908c19e5d7c6f55da5990b5a68d256f916
Ali İlteriş Keskin <ilteriskesk20@gmail.com>
2018-10-01 14:38:19 +0300

    Create new app

--- OnlineMarkdownEditor/OnlineMarkdownEditor/urls.py
+++ OnlineMarkdownEditor/OnlineMarkdownEditor/urls.py
@@ -20,5 +20,6 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
-    path('user/', include("user.urls"))
+    path('user/', include("user.urls")),
+    path('mdfiles/', include('mdfiles.urls')),
 ]
---
+++ OnlineMarkdownEditor/mdfiles/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path,include
+from . import views
+
+app_name ="mdfiles"
+
+urlpatterns =[
+    path('createfiles/',views.createfiles,name="createfiles"),
+]
