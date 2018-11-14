a79be6c2fe0415ad1c00e2c20369be119766e5f0
Ali İlteriş Keskin <ilteriskesk20@gmail.com>
2018-11-15 02:46:08 +0300

    Markdown metni HTML formatında ile görüntülenmeye başlandı.

--- ome/mdfiles/urls.py
+++ ome/mdfiles/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from mdfiles.views import markdown_create, markdown_convert
+from mdfiles.views import markdown_create, markdown_convert, markdown_save
 
 urlpatterns = [
     path("markdown-create/", markdown_create, name="markdown-create"),
     path("markdown-convert/", markdown_convert, name="markdown-convert"),
+    path("markdown-save/", markdown_save, name="markdown-save")
 ]
--- ome/mdfiles/views.py
+++ ome/mdfiles/views.py
@@ -24,3 +24,15 @@ def markdown_convert(request):
             #new_ome.save()
             print(new_ome)
         return render(request, 'markdown_convert.html', context={'form': form, 'new_ome': new_ome})
+
+def markdown_save(request):
+    mdfile = OmeForm(data=request.POST or None)
+    if request.method == "POST":
+        form = OmeForm(data=request.POST)
+        if form.is_valid():
+            new_ome = form.save(commit=False)
+            markdown_text = form.cleaned_data.get('markdown_text')
+            new_ome.html_text = markdown2.markdown(markdown_text)
+            new_ome.save()
+        return render(request, 'markdown_convert.html', context={'form': form})
+    return render(request, 'markdown_convert.html', context={'form': mdfile})
